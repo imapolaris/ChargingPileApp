88dddbca154f78b9fe18716bcffeceba214128a7
sunhui <sunhui@soaring.com.cn>
2017-08-17 14:41:55 +0800

    add icons on tabnavigator by react-native-vector-icons module

--- CustomComponents/Navigators/CPATabNavigator/index.js
+++ CustomComponents/Navigators/CPATabNavigator/index.js
@@ -5,18 +5,30 @@ import {TabNavigator} from 'react-navigation';
 import HomePage from "../../../CustomPages/HomePage/index";
 import MePage from "../../../CustomPages/MePage/index";
 
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+
 const CPATabNavigator = TabNavigator(
     {
         Home: {
             screen: HomePage,
             navigationOptions:{
                 title:'找电桩',
+                tabBarIcon:({tintColor}) => {
+                    return (
+                        <Icon name="home" size={16} color={tintColor} />
+                    );
+                },
             }
         },
         Me: {
             screen: MePage,
             navigationOptions:{
                 title: '我',
+                tabBarIcon: ({tintColor}) => {
+                    return (
+                        <Icon name="user" size={16} color={tintColor} />
+                    );
+                },
             }
         },
     },
@@ -27,6 +39,10 @@ const CPATabNavigator = TabNavigator(
 
         tabBarOptions:{
             activeTintColor: '#e91e63',
+            showIcon:true,
+            tabStyle:{
+                flexDirection:'row',
+            }
         }
     }
 );
