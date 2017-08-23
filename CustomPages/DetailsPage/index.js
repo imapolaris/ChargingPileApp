bba5ac6da3c7d859ff84e5d5004327447aef23d9
sunhui <sunhui@soaring.com.cn>
2017-08-23 10:59:18 +0800

    add tabnavigator to details page

--- CustomPages/DetailsPage/index.js
+++ CustomPages/DetailsPage/index.js
@@ -1,20 +1,89 @@
 import React, {Component} from 'react';
-import {View, ScrollView, Text} from 'react-native';
+import {View, FlatList, Text, ScrollView} from 'react-native';
 
 import styles from './styles';
+import {TabNavigator} from 'react-navigation';
+import {Button} from 'react-native-elements';
 
 class CPADetailsPage extends Component{
+    // 导航
+    _mapNavigation = () => {
+        const {navigate} = this.props.navigation;
+        navigate && navigate('MapNav');
+    };
+
     render() {
         return (
-            <ScrollView style={styles.container}>
-                <View style={styles.container}>
-                    <Text>
-                        Here is Details Page!!!
-                    </Text>
+            <View style={styles.container}>
+                <View style={styles.infoContainer}>
+                    <StationNavigator />
                 </View>
+
+                <View style={styles.actionContainer}>
+                    <Button title="导航"
+                            onPress={this._mapNavigation}
+                            buttonStyle={styles.button}
+                    />
+                </View>
+            </View>
+        );
+    }
+}
+
+// 基本信息
+class CPABasicInfoPage extends Component{
+    render() {
+        return (
+            <ScrollView>
+                <Text>
+                    基本信息
+                </Text>
             </ScrollView>
         );
     }
 }
+
+// 电桩信息
+class CPAElectricPileInfoPage extends Component{
+    render() {
+        return (
+            <ScrollView>
+                <Text>
+                    电桩信息
+                </Text>
+            </ScrollView>
+        );
+    }
+}
+
+const StationNavigator = TabNavigator(
+    {
+        Basic: {
+            screen: CPABasicInfoPage,
+            navigationOptions: {
+                title: '基本信息'
+            },
+        },
+        ElectricPile: {
+            screen: CPAElectricPileInfoPage,
+            navigationOptions: {
+                title: '电桩信息'
+            },
+        }
+    },
+    {
+        tabBarPosition: 'top',
+        swipeEnabled: true,
+        animationEnabled: true,
+
+        tabBarOptions:{
+            activeTintColor: '#e91e63',
+            showIcon:false,
+            tabStyle:{
+                flexDirection:'row',
+            }
+        },
+    }
+);
 
 export default CPADetailsPage;
--- CustomPages/DetailsPage/styles.js
+++ CustomPages/DetailsPage/styles.js
@@ -1,12 +1,20 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    infoContainer: {
         flex: 1,
     },
-    content:{
-        //flex: 1,
+    actionContainer: {
+        padding: 30,
+        alignItems: 'center',
     },
+    button: {
+        width: 150,
+    }
 });
 
 export default styles;
