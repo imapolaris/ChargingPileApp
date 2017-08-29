47a1fe46e8d948ed5f430165d5afc53ab6b82f2c
sunhui <sunhui@soaring.com.cn>
2017-08-29 12:40:48 +0800

    add react-native-camera and react-native-qrcode-scanner components

--- android/app/src/main/java/com/chargingpileapp/MainApplication.java
+++ android/app/src/main/java/com/chargingpileapp/MainApplication.java
@@ -3,6 +3,9 @@ package com.chargingpileapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ImagePickerPackage(),
+            new RCTCameraPackage(),
             new ImagePickerPackage(),
             new VectorIconsPackage()
       );
