d5aaf2d8f31a6ba4c906bb583d5fe4b1d4356ffa
sunhui <sunhui@soaring.com.cn>
2017-08-30 16:10:46 +0800

    fix issues of react-native-yunpeng-alipay library

--- android/app/src/main/java/com/chargingpileapp/MainApplication.java
+++ android/app/src/main/java/com/chargingpileapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.chargingpileapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.yunpeng.alipay.AlipayPackage;
 import com.theweflex.react.WeChatPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AlipayPackage(),
             new WeChatPackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
