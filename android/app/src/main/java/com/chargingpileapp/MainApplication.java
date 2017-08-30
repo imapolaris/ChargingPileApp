602f90a8ac50bd7a03a88e169d855224df93fed5
sunhui <sunhui@soaring.com.cn>
2017-08-30 10:59:25 +0800

    config react-native-wechat & react-native-camera libraries; remove react-native-yunpeng-alipay

--- android/app/src/main/java/com/chargingpileapp/MainApplication.java
+++ android/app/src/main/java/com/chargingpileapp/MainApplication.java
@@ -3,7 +3,6 @@ package com.chargingpileapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.yunpeng.alipay.AlipayPackage;
 import com.theweflex.react.WeChatPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new AlipayPackage(),
             new WeChatPackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
